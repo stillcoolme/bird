00d7a5bd9ce6dd84ff8e7681f5f4128b56e928b6
zhangjianhua <stillcoolman@sina.com>
2019-06-30 15:09:57 +0800

    15_3Sum

---
+++ bird-leetcode/src/main/java/com/stillcoolme/contest/_143_20190630/_1103_BSFindRoad.java
@@ -0,0 +1,23 @@
+package com.stillcoolme.contest._143_20190630;
+
+import java.util.List;
+
+/**
+ * @author: create by stillcoolme
+ * @date: 2019/6/30 10:58
+ * @description:
+ * 在一棵无限的二叉树上，每个节点都有两个子节点，树中的节点 逐行 依次按 “之” 字形进行标记。
+ * 如下图所示，在奇数行中，按从左到右的顺序进行标记；而偶数行中，按从右到左的顺序进行标记。
+ *        1
+ *     3   2
+ *   4 5  6 7
+ * 给你树上某一个节点的标号 label，请你返回从根节点到该标号为 label 节点的路径，该路径是由途经的节点标号所组成的。
+ * 输入：label = 5
+ * 输出：[1,3,5]
+ **/
+public class _1103_BSFindRoad {
+    public List<Integer> pathInZigZagTree(int label) {
+
+        return null;
+    }
+}
