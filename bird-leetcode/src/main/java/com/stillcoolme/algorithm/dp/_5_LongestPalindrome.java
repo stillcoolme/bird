8f9b54f90870e4de64187cb899a48df76c4ab27b
stillcoolme <stillcoolman@sina.com>
2019-08-06 19:59:15 +0800

    322_CoinChange

---
+++ bird-leetcode/src/main/java/com/stillcoolme/algorithm/dp/_5_LongestPalindrome.java
@@ -0,0 +1,28 @@
+package com.stillcoolme.algorithm.dp;
+
+/**
+ * @author: stillcoolme
+ * @date: 2019/8/6 11:44
+ * @description:
+ *  给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为 1000。
+ *  示例 1：
+ *  输入: "babad"
+ *  输出: "bab"
+ *  注意: "aba" 也是一个有效答案。
+ **/
+public class _5_LongestPalindrome {
+
+    /**
+     * dp
+     * @param s
+     * @return
+     */
+    public String longestPalindrome(String s) {
+        return null;
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+}
