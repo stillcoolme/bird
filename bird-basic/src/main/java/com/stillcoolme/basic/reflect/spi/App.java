eb2fc6f57e8f450c99def96594b126d92665f0b8
stillcoolme <stillcoolman@sina.com>
2020-03-30 10:19:27 +0800

    update doc

--- bird-basic/src/main/java/com/stillcoolme/basic/reflect/spi/App.java
+++ bird-basic/src/main/java/com/stillcoolme/basic/reflect/spi/App.java
@@ -7,10 +7,12 @@ import java.util.ServiceLoader;
  * @date: 2020/3/25 15:30
  * Function:
  *   ServiceLoader是SPI的是一种实现，所谓SPI，即Service Provider Interface，用于一些服务提供给第三方实现或者扩展，可以增强框架的扩展或者替换一些组件。
- *   将实现类名称配置在相关项目的固定目录下： resources/META-INF/services/接口全称。
+ *   注意：要将实现类名称配置在 实现类项目 的固定目录下： resources/META-INF/services/接口全称。
  *
  *   这个在大数据的应用中颇为广泛，
  *   比如 spark sql 的外部集群管理器接口 ExternalClusterManager、数据源的接入接口DataSourceRegister
+ *
+ *   https://cloud.tencent.com/developer/article/1110670
  */
 public class App {
 
