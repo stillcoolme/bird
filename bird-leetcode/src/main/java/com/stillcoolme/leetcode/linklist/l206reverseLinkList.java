3c1fedbb7ea237bcec46d8cb41fa9b0b61b2ac0f
zhangjianhua <stillcoolman@sina.com>
2023-07-24 21:57:39 +0800

    lc206

---
+++ bird-leetcode/src/main/java/com/stillcoolme/leetcode/linklist/l206reverseLinkList.java
@@ -0,0 +1,32 @@
+package com.stillcoolme.leetcode.linklist;
+
+import com.stillcoolme.leetcode.data.ListNode;
+
+/**
+ * 2023-07-24 10min做不出来， 不知到往前的指针怎么变成往后？
+ */
+public class l206reverseLinkList {
+
+
+    public ListNode reverseList(ListNode head) {
+        ListNode pre = null;
+        ListNode curr = head;
+
+        //  null (pre)  -> head (curr)  ->  1  (temp)  -> 2  -> 3
+
+        while (curr != null) {
+            ListNode temp = curr.next;    // 让 temp 在前面做前导， 不然 curr 的 next 掉转方向了找不到 前面的节点
+            curr.next = pre;              // curr 往后指
+            pre = curr;                   // curr 往后指完，大家继续前进
+            curr = temp;                  // curr 往后指完，大家继续前进
+
+
+        }
+        return pre;
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+}
