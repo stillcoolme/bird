a8f65d77aaeb6bb10eb3cdd33e39c9f7922d4112
stillcoolme <stillcoolman@sina.com>
2023-07-24 17:55:51 +0800

    链表查找元素

---
+++ bird-leetcode/src/main/java/com/stillcoolme/leetcode/all/l203removeElements.java
@@ -0,0 +1,55 @@
+package com.stillcoolme.leetcode.all;
+
+/**
+ * <p>TODO</p>
+ *
+ * @author stillcoolme
+ * @version V1.0.0
+ * @date 2023/7/20 9:52
+ */
+
+
+public class l203removeElements {
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+    /**
+     * 1. 需要构造多一个头节点！
+     * 2. 不能用赋 null 来将节点置空 temp.next = null
+     * @param head
+     * @param val
+     * @return
+     */
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode temp = new ListNode(0);
+        temp.next = head;
+        ListNode result = temp;
+        while (temp.next != null) {
+            if (temp.next.val == val) {
+                temp.next = temp.next.next;
+            } else {
+                temp = temp.next;
+            }
+        }
+        return result.next;
+    }
+
+    public static void main(String[] args) {
+        ListNode listNode1 = new ListNode(1, null);
+        ListNode listNode2 = new ListNode(2, listNode1);
+        ListNode listNode3 = new ListNode(3, listNode2);
+        ListNode listNode6 = new ListNode(6, listNode3);
+        ListNode listNode5 = new ListNode(5, listNode6);
+        ListNode listNode66 = new ListNode(6, listNode5);
+        l203removeElements l203removeElements = new l203removeElements();
+        ListNode listNode = l203removeElements.removeElements(listNode66, 6);
+        System.out.println(listNode);
+
+    }
+}
