09e889e3183f399a079f09c7801b48b8024f64f1
zhangjianhua <stillcoolman@sina.com>
2023-07-30 00:04:36 +0800

    lc0142

--- bird-leetcode/src/main/java/com/stillcoolme/leetcode/linklist/lc0141_IsCycle.java
+++ bird-leetcode/src/main/java/com/stillcoolme/leetcode/linklist/lc0141_IsCycle.java
@@ -5,7 +5,7 @@ import com.stillcoolme.leetcode.data.ListNode;
 /**
  * 注意：走两步的时候，不要把 null 的 next 赋给节点，导致节点是 null， null.next 会报空指针！！
  */
-public class lc0141isCycle {
+public class lc0141_IsCycle {
 
     public boolean hasCycle(ListNode head) {
         ListNode step1 = head;
@@ -28,7 +28,7 @@ public class lc0141isCycle {
     public static void main(String[] args) {
         ListNode listNode2 = new ListNode(2, null);
         ListNode listNode1 = new ListNode(1, listNode2);
-        lc0141isCycle lc0203removeElements = new lc0141isCycle();
+        lc0141_IsCycle lc0203removeElements = new lc0141_IsCycle();
         System.out.println(lc0203removeElements.hasCycle(listNode1));
 
     }
